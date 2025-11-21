22bd0bb64c2f5b7c2f3a30239405aa3990b038e5
Md.Shehab Al Rabby <shehabrabby764@gmail.com>
2025-11-21 15:31:00 +0600

    Refresh data

--- src/Pages/Home/Dashboard/MyParcels/MyParcels.jsx
+++ src/Pages/Home/Dashboard/MyParcels/MyParcels.jsx
@@ -2,18 +2,49 @@ import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import useAuth from "../../../../Hooks/useAuth";
 import useAxiosSecure from "../../../../Hooks/useAxiosSecure";
+import { FiEdit } from "react-icons/fi";
+import { FaMagnifyingGlass, FaTrashCan } from "react-icons/fa6";
+import Swal from "sweetalert2";
 
 const MyParcels = () => {
   const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
 
-  const { data: parcels = [] } = useQuery({
+  const { data: parcels = [], refetch } = useQuery({
     queryKey: ["my-parcels", user?.email],
     queryFn: async () => {
       const res = await axiosSecure.get(`/parcels?email=${user.email}`);
       return res.data;
     },
   });
+  const handleParcelDelete = (id) => {
+    console.log(id);
+
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/parcels/${id}`).then((res) => {
+          // console.log(res.data);
+          if (res.data.deletedCount) {
+            // refresh the data in the UI 
+            refetch()
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your parcel request has been deleted.",
+              icon: "success",
+            });
+          }
+        });
+      }
+    });
+  };
 
   return (
     <div>
@@ -37,7 +68,20 @@ const MyParcels = () => {
                 <td>{parcel.parcelName}</td>
                 <td>{parcel.cost}</td>
                 <td>Blue</td>
-                <td>Blue</td>
+                <td className="flex gap-2">
+                  <button className="btn btn-square hover:bg-lime-300 gap-2">
+                    <FaMagnifyingGlass className=""></FaMagnifyingGlass>
+                  </button>
+                  <button className="btn btn-square hover:bg-lime-300 flex gap-2">
+                    <FiEdit></FiEdit>
+                  </button>
+                  <button
+                    onClick={() => handleParcelDelete(parcel._id)}
+                    className="btn btn-square hover:bg-lime-300 flex gap-2"
+                  >
+                    <FaTrashCan></FaTrashCan>
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
