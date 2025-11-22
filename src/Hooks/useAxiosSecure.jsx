74849c32e8da98d3853bc1474d57f6801abfa98b
Md.Shehab Al Rabby <shehabrabby764@gmail.com>
2025-11-23 00:17:08 +0600

    JWT token & axios interceptor to send Token to backend

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,10 +1,20 @@
 import axios from "axios";
-import React from "react";
+
+import React, { useEffect } from "react";
+import useAuth from "./useAuth";
 const axiosSecure = axios.create({
   baseURL: "http://localhost:3000",
 });
 
 const useAxiosSecure = () => {
+  const { user } = useAuth();
+  useEffect(() => {
+    // intercept requiest 
+    axiosSecure.interceptors.request.use((config) => {
+      config.headers.Authorization = `Bearer ${user.accessToken}`;
+      return config;
+    });
+  }, [user]);
   return axiosSecure;
 };
 
